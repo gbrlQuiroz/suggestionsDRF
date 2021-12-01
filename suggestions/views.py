40b06751bf263ae770b1904daa035f202c0e5c01
Gabriel Quiroz <gabriel@mb.company>
2021-12-01 15:13:52 -0600

    primer prueba, corrigiendo serializer

--- suggestions/views.py
+++ suggestions/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, CharFilter, BooleanFilter, NumberFilter
 
-# Create your views here.
+from .serializers import *
+
+
+class SuggestionsFilter(FilterSet):
+    q = CharFilter(field_name='name', lookup_expr='icontains')
+    latitude = CharFilter(field_name='latitude', lookup_expr='icontains')
+    longitude = CharFilter(field_name='longitude', lookup_expr='icontains')
+
+    class Meta:
+        model = City
+        fields = ['q', 'latitude', 'longitude']
+
+
+class SuggestionsFilteredListView(ListAPIView):
+    queryset = City.objects.all()
+    serializer_class = SuggestionsFilteredListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SuggestionsFilter
